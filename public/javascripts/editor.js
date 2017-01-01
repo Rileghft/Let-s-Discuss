e1cbdab09499ba510cc67db3ae49c11c491708ba
fnsne <watashiwafnsne@gmail.com>
2017-01-01 22:38:44 +0800

    文字編輯器 加上一些東西

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -3,18 +3,46 @@
  */
 var editor;
 var editor_container;
+var delta;
+var toolbar_options = [
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{'header':[1,2,3,4,5,6,false]}],
+    [{'list': 'ordered'}, {'list': 'bullet'}],
+    [{'script': 'sub'}, {'script': 'super'}],
+    [{'indent': '-1'}, {'indent': '+1'}],
+    [{'direction': 'rtl'}],
+    [{'size': ['small', false, 'large', 'huge']}],
+    ['link', 'image', 'video', 'formula'],
+    [{'color': [] }, {'background': [] } ],
+    [{'font': [] } ],
+    [{'align': [] } ]
+];
 var options = {
     modules:{
-        toolbar: true
+        toolbar: toolbar_options
     },
+    theme: 'snow',
     placeholder: "預設值",
     scrollingContainer: "",
-    theme: 'snow'
 };
+var on_off = true;
 
 document.addEventListener('DOMContentLoaded', function(){
     init();
     create_editor();
+    $('#save_delta').click(function () {
+        delta = editor.getContents();
+    });
+
+    $('#load_delta').click(function () {
+        editor.setContents(delta);
+    });
+
+    $('#on_off_btn').click(function () {
+        on_off = !on_off;
+        editor.enable(on_off);
+    });
 });
 
 function create_editor() {
