aa1d8df66a5d270d4675ce96f3a2b96a7a9b8049
fnsne <watashiwafnsne@gmail.com>
2017-01-02 21:28:01 +0800

    Revert "新增一個簡易的websocket"
    This reverts commit 4c0639128d3899c577f6c48fd8eb6f883d3aafaf.

--- app/controllers/DiscussPage.java
+++ app/controllers/DiscussPage.java
@@ -1,16 +1,8 @@
 package controllers;
 
-import actors.MyWebSocketActor;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.WebSocket;
 import views.html.index;
-import akka.actor.*;
-import play.libs.F.*;
-import play.mvc.WebSocket;
-import play.mvc.LegacyWebSocket;
-import actors.*;
-import java.lang.*;
 
 /**
  * Created by 楊舜宇 on 2016/12/28.
@@ -19,9 +11,5 @@ public class DiscussPage extends Controller{
 
     public Result index() {
         return ok(index.render());
-    }
-
-    public LegacyWebSocket<String> socket(){
-        return WebSocket.withActor(MyWebSocketActor::props);
     }
 }
