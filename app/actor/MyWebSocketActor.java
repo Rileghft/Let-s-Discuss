4f33580b095825c7950a328a2f20f10e54648946
fnsne <watashiwafnsne@gmail.com>
2017-01-02 21:39:15 +0800

    新增簡單的Websocket server

---
+++ app/actor/MyWebSocketActor.java
@@ -0,0 +1,23 @@
+package actor;
+
+import akka.actor.*;
+/**
+ * Created by fnsne on 2017/1/2.
+ */
+public class MyWebSocketActor extends UntypedActor{
+    public static Props props(ActorRef out){
+        return Props.create(MyWebSocketActor.class, out);
+    }
+
+    private final ActorRef out;
+
+    public MyWebSocketActor(ActorRef out){
+        this.out = out;
+    }
+
+    public void onReceive( Object message ){
+        if (message instanceof String){
+            out.tell("I received your message:" + message, self());
+        }
+    }
+}
--- app/controllers/DiscussPage.java
+++ app/controllers/DiscussPage.java
@@ -3,6 +3,11 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import akka.actor.*;
+import play.libs.F.*;
+import play.mvc.WebSocket;
+import play.mvc.LegacyWebSocket;
+import actor.*;
 
 /**
  * Created by 楊舜宇 on 2016/12/28.
@@ -11,5 +16,9 @@ public class DiscussPage extends Controller{
 
     public Result index() {
         return ok(index.render());
+    }
+
+    public LegacyWebSocket<String> socket(){
+        return WebSocket.withActor(MyWebSocketActor::props);
     }
 }
